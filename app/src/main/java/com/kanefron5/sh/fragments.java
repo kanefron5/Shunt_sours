6455728b989de6478b2f2908d900335f1886c233
kanefron5 <kanerfon5@gmail.com>
2015-03-09 18:28:01 +0300

    Initial commit

---
+++ app/src/main/java/com/kanefron5/sh/fragments.java
@@ -0,0 +1,149 @@
+package com.kanefron5.sh;
+
+import android.os.Bundle;
+import android.support.v4.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+class ScreenOne extends Fragment {
+
+    public ScreenOne() {
+    }
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+
+        View rootView = inflater.inflate(R.layout.screen_one, container,
+                false);
+
+        return rootView;
+    }
+
+}
+ class ScreenTwo extends Fragment {
+
+    public ScreenTwo() {
+    }
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+
+        View rootView = inflater.inflate(R.layout.screen_two, container,
+                false);
+
+        return rootView;
+    }
+
+}
+class ScreenThree extends Fragment {
+
+    public ScreenThree() {
+    }
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+
+        View rootView = inflater.inflate(R.layout.screen_three, container,
+                false);
+
+        return rootView;
+    }
+}
+class ScreenFour extends Fragment {
+
+        public ScreenFour() {
+        }
+
+        @Override
+        public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                                 Bundle savedInstanceState) {
+
+            View rootView = inflater.inflate(R.layout.screen_four, container,
+                    false);
+
+            return rootView;
+        }
+    }
+class ScreenFive extends Fragment {
+
+            public ScreenFive() {
+            }
+
+            @Override
+            public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                                     Bundle savedInstanceState) {
+
+                View rootView = inflater.inflate(R.layout.screen_five, container,
+                        false);
+
+                return rootView;
+            }
+        }
+class ScreenSix extends Fragment {
+
+                public ScreenSix() {
+                }
+
+                @Override
+                public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                                         Bundle savedInstanceState) {
+
+                    View rootView = inflater.inflate(R.layout.screen_six, container,
+                            false);
+
+                    return rootView;
+                }
+            }
+class ScreenSeven extends Fragment {
+
+                    public ScreenSeven() {
+                    }
+
+                    @Override
+                    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                                             Bundle savedInstanceState) {
+
+                        View rootView = inflater.inflate(R.layout.screen_seven, container,
+                                false);
+
+                        return rootView;
+                    }
+                }
+class ScreenEight extends Fragment {
+
+                        public ScreenEight() {
+                        }
+
+                        @Override
+                        public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                                                 Bundle savedInstanceState) {
+
+                            View rootView = inflater.inflate(R.layout.screen_eight, container,
+                                    false);
+
+                            return rootView;
+                        }
+                    }
+class ScreenAbout extends Fragment {
+
+                            public ScreenAbout() {
+                            }
+
+                            @Override
+                            public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                                                     Bundle savedInstanceState) {
+
+                                View rootView = inflater.inflate(R.layout.screen_about, container,
+                                        false);
+
+                                return rootView;
+                            }
+                        }
+
+
+
+
